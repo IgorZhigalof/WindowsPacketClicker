10b91c6684f06bb4ac143f0cf4cf3cfe31d6287c
Игорь <hardplayer455@gmail.com>
2025-04-03 21:33:08 +0300

    Rename variables and added a few methods

--- betterWindowBuilder.py
+++ betterWindowBuilder.py
@@ -5,19 +5,36 @@ import ctypes
 class BetterWindowBuilder:
     user32 = ctypes.WinDLL("user32", use_last_error=True)
     title = ""
-    click_time = None
-    press_time = None
-    delay_time = None
+    clickTime = None
+    pressTime = None
+    writeDelayTime = None
 
     def setWindowTitle(self, title: str):
         self.title = title
 
-    def find_windows_by_title(self, title: str) -> int:
+    def findWindowsByTitle(self, title: str) -> int:
         return self.user32.FindWindowExW(None, None, None, title)
 
-    def set_default_delay(self, time: int):
-        self.click_time = time
+    def setClickDelay(self, time: int):
+        self.clickTime = time
+
+    def setPressTime(self, time):
+        self.pressTime = time
+
+    def setWriteDelayTime(self, time):
+        self.pressTime = time
+
+    def setDefaultDelay(self, time: int):
+        self.clickTime = setIfNotNone(self.clickTime, time)
+        self.pressTime = setIfNotNone(self.pressTime, time)
+        self.writeDelayTime = setIfNotNone(self.writeDelayTime, time)
 
     def build(self):
         return BetterWindow(self.find_windows_by_title(self.title), self.time)
 
+
+def setIfNotNone(param, value):
+    if (param != None):
+        return param
+    else:
+        return value
